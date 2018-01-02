cffac93b22034aab81451fc2774409a4b1075bd3
josercc <josercc@163.com>
2018-01-02 10:15:15 +0800

    新增部分功能
    支持自定义每个的样式风格
    支持关闭交互

--- ZHAutoSizeTagView-iOS/ZHAutoSizeTagView-iOS/ZHAutoSizeTagManager.swift
+++ ZHAutoSizeTagView-iOS/ZHAutoSizeTagView-iOS/ZHAutoSizeTagManager.swift
@@ -28,7 +28,22 @@ import UIKit
 	@objc public var textFont:UIFont = UIFont.systemFont(ofSize: 12)
 	@objc public var tagHeight:CGFloat = 0
 	@objc public var defaultSelectedIndex:Int = -1
+	@objc public var subTagConfigBlock:((_ index:Int) -> ZHAutoSizeSubTagConfig?)?
+	@objc public var isUserInteractionEnabled:Bool = true
 	@objc public override init() {
 		super.init()
 	}
+}
+
+public class ZHAutoSizeSubTagConfig: NSObject {
+	@objc public var selectBackgroundColor:UIColor = #colorLiteral(red: 1, green: 0.9294117647, blue: 0.8705882353, alpha: 1)
+	@objc public var selectBoardColor:UIColor = #colorLiteral(red: 1, green: 0.5411764706, blue: 0, alpha: 1)
+	@objc public var selectBoardWidth:CGFloat = 0
+	@objc public var selectTextColor:UIColor = #colorLiteral(red: 1, green: 0.5411764706, blue: 0, alpha: 1)
+	@objc public var selectImage:UIImage? = UIImage(named: "zh_autosize_tag_select_icon", in: Bundle(for: ZHAutoSizeTagManager.self), compatibleWith: nil)
+	@objc public var defaultBackgroundColor:UIColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+	@objc public var defaultBoardColor:UIColor = #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+	@objc public var defaultBoardWidth:CGFloat = 1
+	@objc public var defaultTextColor:UIColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+	@objc public var defaultImage:UIImage?
 }

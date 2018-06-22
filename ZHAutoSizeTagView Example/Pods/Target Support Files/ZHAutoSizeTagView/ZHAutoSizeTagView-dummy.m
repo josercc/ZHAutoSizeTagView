8f1aaa8cd21e148b2caa851b1e4868fba2dd221f
josercc <zhanghang@globalegrow.com>
2018-06-22 17:58:12 +0800

    支持源码和 Framework

--- ZHAutoSizeTagView Example/Pods/Target Support Files/ZHAutoSizeTagView/ZHAutoSizeTagView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZHAutoSizeTagView : NSObject
-@end
-@implementation PodsDummy_ZHAutoSizeTagView
-@end
--- ZHAutoSizeTagView Example/Pods/Target Support Files/ZHAutoSizeTagView/ZHAutoSizeTagView-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double ZHAutoSizeTagViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char ZHAutoSizeTagViewVersionString[];
-
